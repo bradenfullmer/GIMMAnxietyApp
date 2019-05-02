4707dc48fc1899348bd05d57f2a3544786e6aeae
ZacharyDiNucci <46699402+ZacharyDiNucci@users.noreply.github.com>
2019-05-02 13:32:30 -0600

    hopefully one of the last commits

--- MapFirebaseIonic4App/src/app/home/home.page.ts
+++ MapFirebaseIonic4App/src/app/home/home.page.ts
@@ -14,55 +14,52 @@ declare var google;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-@ViewChild('map') mapElement: ElementRef;
-public base64Image: string;
-    locationsList: Array<string> = new Array<string>();
-    location: string;
-map: any;
-position: any;
-locationKey: any;
-currentLoc: Location;
-public Buildings: string;
+  @ViewChild('map') mapElement: ElementRef;
+  public base64Image: string;
+  locationsList: Array<string> = new Array<string>();
+  location: string;
+  map: any;
+  position: any;
+  locationKey: any;
+  public Buildings: string;
 
+  constructor(private router: Router, private geolocation: Geolocation, public firebaseService: FirebaseService) { }
 
-  constructor(private router: Router, private geolocation: Geolocation,
-  public firebaseService: FirebaseService) {
-      //this.locationsList = FirebaseService.getStaticLocation();
-      //console.log(this.locationsList);
-      //this.location = this.locationsList.BuildingName;
-      this.geolocation.getCurrentPosition().then(pos => {
-      this.currentLoc.Lat = pos.coords.latitude;
-      this.currentLoc.Long = pos.coords.longitude;
+  ngOnInit() {
+    let mapOptions = {
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false,
+      streetViewControl: false,
+      fullScreenControl: false
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    this.geolocation.getCurrentPosition().then(pos => {
+      let latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng,
+        icon: {
+          url: 'http://maps.google.com/mapfiles/kml/shapes/firedept.png'
+      }
+      });
     }).catch((error) => {
       console.log('Error getting location', error);
     });
-  }
 
-    ngOnInit() {
+    this.firebaseService.checkBuilding();
+    console.log(FirebaseService.getPosList());
 
-        //this.locationsList = FirebaseService.staticBuilding;
-        //console.log("THE " + this.locationsList);
-        //this.location = this.locationsList.BuildingName.valueOf();
-
-        let mapOptions = {
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullScreenControl: false
-        }
-        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-        this.firebaseService.checkBuilding();
-        console.log(FirebaseService.getPosList());
-
-        for (let item of FirebaseService.getPosList()) {
-            console.log(item.BuildingName);
-            this.addMarker(item);
-            this.locationsList.push(item.BuildingName);
-            this.position = new google.maps.LatLng(item.Lat, item.Long);
-            this.map.setCenter(this.position);
-        }
+    for (let item of FirebaseService.getPosList()) {
+      console.log(item.BuildingName);
+      this.addMarker(item);
+      this.locationsList.push(item.BuildingName);
+      this.position = new google.maps.LatLng(item.Lat, item.Long);
+      this.map.setCenter(this.position);
+      console.log("here");
+    }
   }
   //onContextChange(ctxt: string): void {
   //this.locationsList$ = this.firebaseService.getLocationsList().snapshotChanges().map(changes => {
@@ -70,51 +67,51 @@ public Buildings: string;
   //    key: c.payload.key, ...c.payload.val()
   //  }));
   //});
-//}
-addMarker(location: any){
-  let latLng = new google.maps.LatLng(location.Lat, location.Long);
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: latLng
-  });
+  //}
+  addMarker(location: any) {
+    let latLng = new google.maps.LatLng(location.Lat, location.Long);
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng
+    });
 
-  this.addInfoWindow(marker, location);
-}
-assignLocation(loc: Location){
-  this.firebaseService.setCurrentLocation(loc);
-  this.currentLoc = loc;
-  this.locationKey = loc.Key;
-  console.log("Assigned location key: " + this.locationKey);
-}
-addInfoWindow(marker, location){
-  let contentString = '<div class="info-window" id="clickableItem" >' +
-     '<h3>' +'</h3>' +
-     '<div class="info-content">' +
-     '<img src="'+ '" style="width:30px;height:30px;border-radius: 50%; padding: 20px, 20px, 20px, 20px;"/>' +
-     '<p>' + location.BuildingName + '</p>' +
-     '</div>' +
-     '</div>';
+    this.addInfoWindow(marker, location);
+  }
+  assignLocation(loc: Location) {
+    this.firebaseService.setCurrentLocation(loc);
+    this.locationKey = loc.Key;
+    console.log("Assigned location key: " + this.locationKey);
+  }
+  addInfoWindow(marker, location) {
+    let contentString = 
+      '<div class="info-window" id="clickableItem" >' +
+        '<h3>' + '</h3>' +
+        '<div class="info-content">' +
+          '<img src="' + '" style="width:30px;height:30px;border-radius: 50%; padding: 20px, 20px, 20px, 20px;"/>' +
+          '<p>' + location.BuildingName + '</p>' +
+        '</div>' +
+      '</div>';
 
-     let infoWindow = new google.maps.InfoWindow({
-       content: contentString,
-       maxWidth: 400
-     });
+    let infoWindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 400
+    });
 
-     google.maps.event.addListener(infoWindow, 'domready', () => {
-       var clickableItem = document.getElementById('clickableItem');
-       clickableItem.addEventListener('click', () => {
-         console.log("clicked on marker");
-         this.firebaseService.setCurrentLocation(location);
-         this.Buildings = location.BuildingName;
-         this.router.navigate(['/list', this.Buildings]);
-       });
-     });
-     google.maps.event.addListener(marker, 'click',() => {
-       infoWindow.open(this.map, marker);
-     });
-     google.maps.event.addListener(this.map, 'click', () => {
-       infoWindow.close(this.map, marker);
-     });
-}
+    google.maps.event.addListener(infoWindow, 'domready', () => {
+      var clickableItem = document.getElementById('clickableItem');
+      clickableItem.addEventListener('click', () => {
+        console.log("clicked on marker");
+        this.firebaseService.setCurrentLocation(location);
+        this.Buildings = location.BuildingName;
+        this.router.navigate(['/list', this.Buildings]);
+      });
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+    google.maps.event.addListener(this.map, 'click', () => {
+      infoWindow.close(this.map, marker);
+    });
+  }
 }
